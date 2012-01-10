a5b740281ada66cce16100605858faeca3081b5f
Dan <Dan@Dinkydetails.com>
2012-01-09 21:26:55 -0500

    Fixed some code in my JSON import and my RC app

--- annis_dan_project2.1/js/main.js
+++ annis_dan_project2.1/js/main.js
@@ -30,7 +30,7 @@ $.getJSON('xhr/taskList.json', function(data) {
   var items = [];
 
   $.each(data, function(key, val) {
-    items.push('<li id="' + key + '">' + val + '</li>');
+    items.push('<li class="ui-li ui-li-static ui-body-d" id="' + key + '">' + val +'</li>');
   });
 
   $('<li/>', {
